0df187f4c197ab6a62062e980eca49e99f97acd8
kruser <kruser@yandex.ru>
2021-11-25 12:46:45 +0500

    space_bot
    publish photo to Telegram channel.

--- space_bot.py
+++ space_bot.py
@@ -1,12 +1,10 @@
 import telegram
+from main import *
 
-TOKEN = '2119927034:AAH67gluVcV1K3wgYC6jVLeaO88Pmyfe0w0'
+
+TOKEN = os.getenv('BOT_TOKEN')
 
 bot = telegram.Bot(token=TOKEN)
 
-print(bot.get_me())
-
-updates = bot.get_updates()
-print(updates[0])
-
-bot.send_message(chat_id='@spaceimages_new', text="Первый пост Бота в канал")
+# bot.send_message(chat_id='@spaceimages_new', text="Первый пост Бота в канал")
+bot.send_document(chat_id='@spaceimages_new', document=open('images/1.jpg', 'rb'))
