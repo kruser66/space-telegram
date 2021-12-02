8047bb14ff82a78aee885fbe37bc9ca11dbb3f4e
kruser <kruser@yandex.ru>
2021-12-02 21:16:59 +0500

    Update core.py
    Decomposited function SOLID-rules

--- core.py
+++ core.py
@@ -10,9 +10,11 @@ def extarct_the_extension(url):
     return os.path.splitext(urlsplit(url).path)[-1]
 
 
-def download_image(image_url, image_name):
-    os.makedirs(IMAGES_DIR, exist_ok=True)
+def make_images_dir(images_dir):
+    os.makedirs(images_dir, exist_ok=True)
 
+
+def download_image(image_url, image_name):
     print('Сохраняем картинку по ссылке {}'.format(image_url.split('?')[0]))
     filename = f'{image_name}{extarct_the_extension(image_url)}'
     response = requests.get(image_url)
@@ -22,6 +24,11 @@ def download_image(image_url, image_name):
         print('Картинка сохранена')
 
 
-if __name__ == '__main__':
+def main():
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    make_images_dir(IMAGES_DIR)
     download_image(url, 'habble')
+
+
+if __name__ == '__main__':
+    main()
